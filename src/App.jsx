d0040611f7bcfa9dabdead9730f7282e881e6952
Karan Jamwal <karanjamwal2004@gmail.com>
2026-01-01 11:29:48 +0530

    implement Redux store and counter slice with basic UI for counter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import './App.css'
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/features/counterSlice";
+import { useState } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  const [num, setNum] = useState(5);
+  const counter = useSelector((state) => state.counter.value);
 
   return (
     <>
       <h1>Vite + React</h1>
+      <h2>Counter: {counter}</h2>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <input
+        value={num}
+        type="number"
+        onChange={(e) => setNum(e.target.value)}
+      />
+      <button onClick={() => dispatch(incrementByAmount(Number(num)))}>
+        Increment by Amount
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
